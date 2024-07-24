c519fd2ef370f6599d2eb7b86ffbfec778e51c49
c0derL1sta <maynardsean676@gmail.com>
2024-07-24 17:32:08 -0400

    Update main.py

--- myGame/main.py
+++ myGame/main.py
@@ -3,52 +3,76 @@ import os
 import pygame
 import player
 
-def main():
-    """this function is called when the program starts.
-    it initializes everything it needs, then runs in
-    a loop until the function returns."""
-    # Initialize Everything
-    pygame.init()
-    screen = pygame.display.set_mode((1000, 800))
-    pygame.display.set_caption("Math Shape survival")
-    pygame.mouse.set_visible(False)
+class game:
+    def __init__(self):
+        """this function is called when the program starts.
+        it initializes everything it needs, then runs in
+        a loop until the function returns."""
+        # Initialize Everything
+        pygame.init()
+        self.screen = pygame.display.set_mode((1000, 800))
+        pygame.display.set_caption("Math Shape survival")
+        pygame.mouse.set_visible(False)
 
-    # Create The Background
-    background = pygame.Surface(screen.get_size())
-    background = background.convert()
-    background.fill((170, 238, 187))
+        # Create The Background
+        background = pygame.Surface(self.screen.get_size())
+        background = background.convert()
+        background.fill((170, 238, 187))
 
-    # Display The Background
-    screen.blit(background, (0, 0))
-    pygame.display.flip()
+        # Display The Background
+        self.screen.blit(background, (0, 0))
+        pygame.display.flip()
 
-    #render spirte?
-    player = player.player(player,900,800)
-    allsprites = pygame.sprite.RenderPlain((player))
-    clock = pygame.time.Clock()
+        #render player sprite?
+        self.player = player.player(self,900,800)
+        self.clock = pygame.time.Clock()
 
+    def main(self):
+        """this function is called when the program starts.
+        it initializes everything it needs, then runs in
+        a loop until the function returns."""
+        # Initialize Everything
+        pygame.init()
+        self.screen = pygame.display.set_mode((1000, 800))
+        pygame.display.set_caption("Math Shape survival")
+        pygame.mouse.set_visible(False)
 
-    # Main Loop
-    playing = True
-    while playing:
-        clock.tick(60)
+        # Create The Background
+        self.background = pygame.Surface(self.screen.get_size())
+        self.background = self.background.convert()
+        self.background.fill((170, 238, 187))
 
-        # Handle Input Events
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                playing = False
-            elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-                playing = False
+        # Display The Background
+        self.screen.blit(self.background, (0, 0))
+        pygame.display.flip()
 
-        #Update all them sprites
-        allsprites.update()
-
-        # Draw Everything
-        screen.blit(background, (0, 0))
-        allsprites.draw(screen)
-        playing.display.flip()
-
-    playing.quit()
+        #render spirte?
+        self.allsprites = pygame.sprite.RenderPlain((self.player))
 
 
-if __name__ == '__main__': main()
+        # Main Loop
+        playing = True
+        while playing:
+            self.clock.tick(60)
+
+            # Handle Input Events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    playing = False
+                elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                    playing = False
+
+            #Update all them sprites
+            self.allsprites.update()
+
+            # Draw Everything
+            self.screen.blit(self.background, (0, 0))
+            self.allsprites.draw(self.screen)
+            pygame.display.flip()
+
+        playing.quit()
+
+
+g = game()
+g.main()
+os.exit()
