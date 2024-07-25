4b72a4c4caceac905f703f23b0fe2a57e9c1c414
c0derL1sta <maynardsean676@gmail.com>
2024-07-25 10:56:22 -0400

    Update player.py

--- myGame/player.py
+++ myGame/player.py
@@ -3,8 +3,6 @@ import os
 
 # classes for our game objects
 class player(pygame.sprite.Sprite):
-    """moves a clenched fist on the screen, following the mouse"""
-
     def __init__(self, game, xPos, yPos):
         # Call the parent class (Sprite) constructor
         pygame.sprite.Sprite.__init__(self)
@@ -18,11 +16,40 @@ class player(pygame.sprite.Sprite):
 
     def move(self):
         # Handle Input Events
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_w:
-                    self.y += 100
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            self.y -= 50
+        if keys[pygame.K_s]:
+            self.y += 50
+        if keys[pygame.K_a]:
+            self.x -= 50
+        if keys[pygame.K_d]:
+            self.x += 50
+    
+    def preventLeavingScreen(self):
+        #A prevention measure to keep player inside the 'screen' and not going out of sight
+        
+        #Dont go to far left
+        if(self.x < 0):
+            self.x = 0
+        
+        #Dont go to far right , subtract player's width so it doesnt look part of it is off the screen
+        elif(self.x > 1000):
+            self.x = 1000 - self.image.get_width()
+        
+        #Dont go to far up, 
+        elif(self.y < 0):
+            self.y = 0
+        
+        #Dont go to far down, subtract player's height so it doesnt look a part of it is off
+        elif(self.y > 800):
+            self.y = 800 - self.image.get_height()
+
 
     def update(self):
         self.move()
+        self.preventLeavingScreen()
+        self.rect.x = self.x
+        self.rect.y = self.y
+        
 
