2ccf5df8045a375b285a7d33509b9af1dc7e0797
c0derL1sta <maynardsean676@gmail.com>
2024-07-26 17:03:27 -0400

    Add files via upload

--- myGame/enemy.py
+++ myGame/enemy.py
@@ -1,7 +1,8 @@
 import pygame
 import math
+import random
 
-class mainAtk(pygame.sprite.Sprit):
+class mainAtk(pygame.sprite.Sprite):
     def __init__(self, Xpos, Ypos):
         pygame.sprite.Sprite.__init__(self)
         self.isDone = False
@@ -9,18 +10,30 @@ class mainAtk(pygame.sprite.Sprit):
         self.x = Xpos
         self.y = Ypos
 
-        #Spawn hintNode with same cordinates 
+        #Spawn attack with same cordinates 
 
-        #Spawn atkNode randomly that has a random
+        #Spawn hint randomly that has a random
+
+        
     
     def getIsCompleted(self):
         return self.isDone
+    
+    def attack(self):
+        if not self.isDone:
+            #Create the 3 random  raidus line with a random angle between 0 and 2pi radians
+            #This will create the 3 hint nodes
+            line1 = 0
 
-class atkNode:
+    def update(self):
+        self.attack()
+
+class atkNode():
     pass
 
-class hintNode:
-    pass
+class hintNode(pygame.sprite.Sprite):
+    def __init__(self):
+        pass
 
 class deathCircle:
     pass
